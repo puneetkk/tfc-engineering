0b0dc28d37fbac045544d5d7ccc7008d1a7d16e0
puneetkk <puneet.kankane@gmail.com>
2023-08-17 02:35:41 +0100

    Added variables root file

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {}
+variable "location" {}
+variable "virtual_network_name" {}
+variable "virtual_network_cidr" {}
+variable "subnet_name" {}
+variable "subnet_cidr" {}
+variable "virtual_machine_name" {}
+variable "admin_username" {}
+variable "admin_password" {}
